e148c5af6cbd28f06973fe3dab8f2f881a665961
BryanFinanci <bryanfinanci@gmail.com>
2026-05-30 18:59:52 -0300

    feat(cadastro): integra formulario com authService preparando para o backend

--- js/pages/cadastro.js
+++ js/pages/cadastro.js
@@ -1,12 +1,14 @@
 import { validarCamposCadastro } from "../utils/validation.js";
+import { cadastrarUsuarioService } from "../services/authService.js";
 
 
 const inputEmail = document.getElementById("email");
 const inputNome = document.getElementById("nome");
 const inputSenha = document.getElementById("senha");
+const paragrafoErro = document.getElementById("paragrafo");
 const botaoCriar = document.getElementById("btnCriar");
 
-function cadastrarUsuario(event){
+async function cadastrarUsuario(event){
     event.preventDefault();
 
     const nomeLimpo = inputNome.value.trim();
@@ -16,6 +18,34 @@ function cadastrarUsuario(event){
    if(!validarCamposCadastro(nomeLimpo,emailLimpo,senhaLimpo)){
     return;
    } 
+
+   const dadosUsuario = {
+    nome: nomeLimpo,
+    email: emailLimpo,
+    senha: senhaLimpo
+   };
+
+   try {
+    const resposta = await cadastrarUsuarioService(dadosUsuario);
+    if(resposta.ok){
+        paragrafoErro.textContent = "Usuario cadastrado com sucesso! Redirecionando...";
+        paragrafoErro.style.color = "2ecc71";
+
+        setTimeout(() => {
+            window.location.href = "/pages/login.html";
+        }, 2000);
+    } else {
+        const erroServidor = await resposta.json();
+        paragrafoErro.textContent = erroServidor.mensagem || "Erro ao cadastrar usuario";
+        paragrafoErro.style.color = "ff4d4d";
+    }
+    
+   } catch (error) {
+    paragrafoErro.textContent = "Não foi possivel conectar ao servidor";
+    paragrafoErro.style.color = "ff4d4d";
+   }
+
+
 }
 
 botaoCriar.addEventListener("click", cadastrarUsuario);
